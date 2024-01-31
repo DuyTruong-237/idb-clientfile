e3ebaad7c8c1191b3a38bbe38229daf331b2c852
DuyTruong-080 <113657419+DuyTruong-237@users.noreply.github.com>
2024-01-31 16:28:01 +0700

    #

--- WebApplication1/Controllers/ClientController.cs
+++ WebApplication1/Controllers/ClientController.cs
@@ -2,96 +2,126 @@
 using Microsoft.EntityFrameworkCore;
 using OfficeOpenXml;
 using System.Threading.Tasks;
+using System.IO;
+using System.Collections.Generic;
+using WebApplication1.Data;
+using WebApplication1.Models;
+using System.Text.RegularExpressions;
+
 namespace WebApplication1.Controllers
-
 {
-    using global::WebApplication1.Data;
-    using global::WebApplication1.Models;
-    using Microsoft.AspNetCore.Mvc;
-
-
-    namespace WebApplication1.Controllers
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientsController : ControllerBase
     {
-        [Route("api/[controller]")]
-        [ApiController]
-        public class ClientsController : ControllerBase
+        private readonly AppDbContext _context;
+
+        public ClientsController(AppDbContext context)
         {
-            private readonly AppDbContext _context;
-
-            public ClientsController(AppDbContext context)
-            {
-                _context = context;
-            }
-
-            // GET: api/PurchaseRequests
-            [HttpGet]
-            public async Task<ActionResult<IEnumerable<Client>>> GetPurchaseRequests()
-            {
-                return await _context.app_fd_info_client.ToListAsync();
-            }
-            [HttpPost("upload")]
-            public async Task<IActionResult> UploadExcelFile(IFormFile file)
-            {
-                ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
-
-                if (file == null || file.Length == 0)
-                {
-                    return BadRequest("File is null or empty");
-                }
-
-                if (!Path.GetExtension(file.FileName).Equals(".xlsx", System.StringComparison.OrdinalIgnoreCase))
-                {
-                    return BadRequest("Invalid file format. Please upload a valid Excel file.");
-                }
-
-                // Đọc dữ liệu từ file Excel
-                using (var stream = new MemoryStream())
-                {
-                    await file.CopyToAsync(stream);
-                    // Bạn có thể thực hiện xử lý dữ liệu ở đây, ví dụ: đọc dữ liệu từ Excel sử dụng thư viện như EPPlus.
-                    using (var package = new ExcelPackage(stream))
-                    {
-                        ExcelWorksheet worksheet = package.Workbook.Worksheets[0];
-                        int rowCount = worksheet.Dimension.Rows;
-
-                        for (int row = 2; row <= rowCount; row++)
-                        {
-                            var soHopDong = worksheet.Cells[row, 1].Value?.ToString().Trim();
-                            var name = worksheet.Cells[row, 2].Value?.ToString().Trim();
-                            var cmnd = worksheet.Cells[row, 3].Value?.ToString().Trim();
-                            var dob = worksheet.Cells[row, 4].Value?.ToString().Trim();
-                            var phone = worksheet.Cells[row, 5].Value?.ToString().Trim();
-                            var address = worksheet.Cells[row, 6].Value?.ToString().Trim();
-                            var tongno = worksheet.Cells[row, 7].Value?.ToString().Trim();
-                            var hangthang = worksheet.Cells[row, 8].Value?.ToString().Trim();
-                            // Tạo đối tượng từ dữ liệu đọc được
-                            var yourDataObject = new Client
-                            {
-                                C_IdContract = soHopDong,
-                                C_Name = name,
-                                C_CMND=cmnd,
-                                C_DayOfBirth = dob,
-                                C_Phone = phone,
-                                C_Address=address,
-                                C_Totalliabilities=tongno,
-                                C_AmountMonthly=hangthang
-                               
-                            };
-
-                            // Lưu vào cơ sở dữ liệu
-                            _context.app_fd_info_client.Add(yourDataObject);
-                        }
-
-                        await _context.SaveChangesAsync();
-                    }
-                }
-
-                // Thực hiện các bước xử lý khác nếu cần
-
-                return Ok("File uploaded successfully");
-            }
-            // Thêm các API khác ở đây nếu cần
+            _context = context;
         }
+
+        // GET: api/PurchaseRequests
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Client>>> GetPurchaseRequests()
+        {
+            return await _context.app_fd_info_client.ToListAsync();
+        }
+
+        [HttpPost("upload")]
+        public async Task<IActionResult> UploadExcelFile(IFormFile file)
+        {
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+            if (file == null || file.Length == 0)
+            {
+                return BadRequest("File is null or empty");
+            }
+
+            if (!Path.GetExtension(file.FileName).Equals(".xlsx", System.StringComparison.OrdinalIgnoreCase))
+            {
+                return BadRequest("Invalid file format. Please upload a valid Excel file.");
+            }
+            string ValidateVietnameseMobileNumber(string number)
+            {
+                string pattern = @"^(09[0-9]|03[2-9]|07[0-9]|08[1-9]|05[6-9])[0-9]{7}$";
+
+                return Regex.IsMatch(number, pattern) ? number : null;
+            }
+            using (var stream = new MemoryStream())
+            {
+                await file.CopyToAsync(stream);
+                using (var package = new ExcelPackage(stream))
+                {
+                    ExcelWorksheet worksheet = package.Workbook.Worksheets[0];
+                    int rowCount = worksheet.Dimension.Rows;
+                    int colCount = worksheet.Dimension.Columns;
+
+                    Dictionary<string, int> columnMappings = new Dictionary<string, int>
+                    {
+                        { "Số hợp đồng", -1 },
+                        { "Tên khách hàng", -1 },
+                        { "CMND", -1 },
+                        { "Ngày sinh", -1 },
+                        { "SDT", -1 },
+                        { "Địa chỉ", -1 },
+                        { "Tổng nợ", -1 },
+                        { "Số tiền cần thanh toán hàng tháng", -1 }
+                    };
+
+                    for (int col = 1; col <= colCount; col++)
+                    {
+                        var cellValue = worksheet.Cells[1, col].Value?.ToString().Trim();
+                        if (columnMappings.ContainsKey(cellValue))
+                        {
+                            columnMappings[cellValue] = col;
+                        }
+                    }
+
+                    for (int row = 2; row <= rowCount; row++)
+                    { 
+                        
+                        var soHopDong = GetCellValue(worksheet, row, columnMappings["Số hợp đồng"]);
+                        if (soHopDong == null)
+                            continue;
+                        var name = GetCellValue(worksheet, row, columnMappings["Tên khách hàng"]);
+                        var cmnd = GetCellValue(worksheet, row, columnMappings["CMND"]);
+                        var dob = GetCellValue(worksheet, row, columnMappings["Ngày sinh"]);
+                        var phone = GetCellValue(worksheet, row, columnMappings["SDT"]);
+                        if (ValidateVietnameseMobileNumber(phone) == null)
+                            phone = "";
+                        var address = GetCellValue(worksheet, row, columnMappings["Địa chỉ"]);
+                        var tongno = GetCellValue(worksheet, row, columnMappings["Tổng nợ"]);
+                        var hangthang = GetCellValue(worksheet, row, columnMappings["Số tiền cần thanh toán hàng tháng"]);
+
+                        var client = new Client
+                        {
+                            C_IdContract = soHopDong,
+                            C_Name = name,
+                            C_CMND = cmnd,
+                            C_DayOfBirth = dob,
+                            C_Phone = phone,
+                            C_Address = address,
+                            C_Totalliabilities = tongno,
+                            C_AmountMonthly = hangthang
+                        };
+
+                        _context.app_fd_info_client.Add(client);
+                    }
+
+                    await _context.SaveChangesAsync();
+                }
+            }
+
+            return Ok("File uploaded successfully");
+        }
+
+        private string GetCellValue(ExcelWorksheet worksheet, int row, int col)
+        {
+            if (col == -1) return null;
+            return worksheet.Cells[row, col].Value?.ToString().Trim();
+        }
+
+        // Thêm các API khác ở đây nếu cần
     }
 }
-
